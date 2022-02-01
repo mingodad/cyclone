b148e8d1ae79c56aa21d0bbd4c4f36880d99bbd8
Domingo Alvarez Duarte <mingodad@gmail.com>
2022-02-01 17:49:19 +0100

    Cleanup unused code

--- mk-it-32.sh
+++ mk-it-32.sh
@@ -3,7 +3,4 @@ MYCC="gcc -m32 -g"
 MYLDFLAGS="-m32 -g -lpthread"
 MYBUILD="i686-pc-linux-gnu"
 MYTARGET="$MYBUILD"
-MYLIB_INSTALL="/home/mingo/dev/c/A_programming-languages/cyclone-dad.git/bin/lib"
-#make CC="$MYCC" TARGET_CC="$MYCC" LDFLAGS="$MYLDFLAGS" build="$MYBUILD" target="$MYTARGET" LIB_INSTALL="$MYLIB_INSTALL" $*
 make CC="$MYCC" TARGET_CC="$MYCC" LDFLAGS="$MYLDFLAGS" build="$MYBUILD" target="$MYTARGET" $*
-#make  LDFLAGS="$MYLDFLAGS" LIB_INSTALL="$MYLIB_INSTALL" $*
